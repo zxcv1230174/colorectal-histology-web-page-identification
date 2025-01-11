99dbafaddd08e68154397f8df5bc5fdca0bed3a0
zxcv1230174 <101238949+zxcv1230174@users.noreply.github.com>
2025-01-11 11:16:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import os
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from tensorflow.keras import layers, models, callbacks, regularizers
+from tensorflow.keras import layers, models, callbacks
 
 # 設置環境變數
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["PYTHONIOENCODING"] = "utf-8"
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # 載入數據集
 [train_ds, valid_ds, test_ds], info = tfds.load('colorectal_histology',
@@ -19,12 +20,22 @@ img_size = input_shapes[:2]
 batch_size = 32
 num_classes = info.features['label'].num_classes
 epoch = 50
+current_dir = os.path.dirname(__file__)
+model_architecture = os.path.join(current_dir, 'DenseNet121_model.keras')
 
 # 預處理函數
-def preprocess_data(data):
-    image = data['image']
-    label = data['label']
-    image = tf.image.resize(image, img_size) / 255.0 # 將圖片大小調整為指定大小,並正規化像素值至 [0, 1]
+def preprocess_data(image_dict):
+    image = tf.image.resize(image_dict['image'], img_size)
+    image = tf.cast(image, tf.float32) / 255.0 #將原本資料集的資料類型從uint8轉換成float32是為了符合我們平常使用的圖片
+    
+    # 計算每個通道的均值和標準差
+    mean = tf.reduce_mean(image, axis=(0, 1), keepdims=True) #計算影像中每個通道（R、G、B）的均值（mean）
+    std = tf.math.reduce_std(image, axis=(0, 1), keepdims=True) #計算影像中每個通道的標準差
+    
+    # 色彩標準化
+    image = (image - mean) / (std + 1e-7) #此步驟通過從每個像素值中減去該通道的均值，並除以標準差來標準化影像的每個通道，1e-7的作用是防止標準差為零時出現除零錯誤。
+    
+    label = image_dict['label']
     return image, label
 
 # 將預處理函數應用到數據集，並將資料分批
@@ -55,7 +66,7 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=
 model.fit(train_data, epochs=epoch, validation_data=valid_data, callbacks=[early_stopping])
 
 # 保存完整模型
-model.save('C:\\Users\\ab090\\Desktop\\TEST\\DenseNet121_model.keras')
+model.save(model_architecture)
 
 # 測試集評估
 test_loss, test_acc = model.evaluate(test_data)
